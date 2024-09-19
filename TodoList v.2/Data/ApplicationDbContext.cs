4157c73ee910c89db7318e281c0e70b5c7236cdf
Martyian <Ivan.Martyian@audius.de>
2024-09-19 16:49:32 +0200

    FirstCommit

---
+++ TodoList v.2/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoList_v._2.Models;
+
+namespace TodoList_v._2.Data
+{
+    
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ TodoList v.2/Models/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoList_v._2.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? CompletedDate { get; set; }
+    }
+}
